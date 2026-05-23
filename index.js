04e7cf55a75680165012ada08dde865aa799356d
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2026-05-22 23:40:04 -0400

    Added sound to all drums and change text color

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 var drums = document.querySelectorAll(".drum");
+
 //forEach loop
 drums.forEach((drum)=>{
-drum.addEventListener("click", ()=>{
-    var audio = new Audio ("sounds/tom-1.mp3");
-    audio.play();
+    drum.addEventListener("click", (e)=>{
+    var key = e.target.innerHTML;
+        // Only change text color to white if the drum is clicked
+        if (key === "w" || key === "a" || key === "s" || key === "d" || key === "j" || key === "k" || key === "l") {
+            e.target.style.color = "blue";
+        }
+
+     if(e.target.innerHTML == "w"){
+           var audio = new Audio ("sounds/tom-1.mp3");
+            audio.play();
+           
+     }else if (e.target.innerHTML == "a"){
+            var audio = new Audio ("sounds/tom-2.mp3");
+            audio.play();
+            
+     }else if (e.target.innerHTML == "s"){
+            var audio = new Audio ("sounds/tom-3.mp3");
+            audio.play();
+        
+     }else if (e.target.innerHTML == "d"){
+            var audio = new Audio ("sounds/tom-4.mp3");
+            audio.play();
+
+     }else if (e.target.innerHTML == "j"){
+            var audio = new Audio ("sounds/crash.mp3");
+            audio.play();
+     }else if (e.target.innerHTML == "k"){
+            var audio = new Audio ("sounds/kick-bass.mp3");
+            audio.play();
+     }else if(e.target.innerHTML == "l"){
+         var audio = new Audio ("sounds/snare.mp3");
+            audio.play();
+     }else{
+        console.log("Please click a drum!")
+     }
+
+    })
 })
-})
+
+
+// Using Switch
+// var btnInnerHTML = e.target.innerHTML;
+// switch (btnInnerHTML) {
+//     case "w":
+//         var audio = new Audio ("sounds/tom-4.mp3");
+//             audio.play();
+//         break;
+
+//     default:
+//         console.log("Please click a drum!")
+      
+// }
+
+
+
+
 
 
 //for loop
@@ -28,23 +80,3 @@ drum.addEventListener("click", ()=>{
 // }
 
 
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function subtract(num1, num2){
-    return num1 - num2;
-}
-function divide(num1, num2){
-    return num1 / num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function calculator(num1, num2, calculate){
-    return calculate(num1, num2)
-}
-
-console.log(calculator(5,2, divide))
