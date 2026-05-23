9afb379f9b4d37daa1829b8466d0bacace25c8a4
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2026-05-22 22:00:16 -0400

    Added sound and background image to the drum kit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var drums = document.querySelectorAll(".drum");
-
-function handleClick(){
-    alert("I got clicked!");
-}
+//forEach loop
+drums.forEach((drum)=>{
+drum.addEventListener("click", ()=>{
+    var audio = new Audio ("sounds/tom-1.mp3");
+    audio.play();
+})
+})
 
 
 //for loop
@@ -12,14 +15,6 @@ function handleClick(){
 // });
     
 // }
-
-
-//forEach loop
-drums.forEach((drum)=>{
-drum.addEventListener("click", ()=>{
-    alert("I got clicked");
-})
-})
 
 
 //while loop
@@ -32,69 +27,24 @@ drum.addEventListener("click", ()=>{
 //    i++; 
 // }
 
-// drum.addEventListener("click", ()=> handleClick())
 
-// drum.addEventListener("click", handleClick);
-
-// var names = ["Mike", "Tony", "Lake"];
-
-// function whoIsPaying(list) {
-//     return list[1]; // Returns the first item from the array passed in
-// }
-
-// var personPaying = whoIsPaying(names); // Pass the 'names' array into the function
-
-// console.log(personPaying); // Outputs: Mike
-
-
-
-// var names = ["Mike", "Tony", "Lake"];
-
-// function pickRandomName(list) {
-//     // 1. Generate a random index between 0 and the list length minus 1
-//     var randomIndex = Math.floor(Math.random() * list.length);
-    
-//     // 2. Return only the name at that random index position
-//     return list[randomIndex]; 
-// }
-
-// var winner = pickRandomName(names);
-// console.log(winner); // Outputs a different random name each time, e.g., "Tony"
-
-
-
-// var names = ["Mike", "Tony", "Lake"];
-
-// function logAllNames(list) {
-//     // The loop goes through each item one by one
-//     for (var name of list) {
-//         console.log("Hello, " + name);
-//     }
-//     // No return statement needed here because we are just printing, not creating data
-// }
-
-// logAllNames(names);
-var names = ["Mike", "Tony", "Lake"];
-names.forEach((name)=>{
-    console.log("Mr " + name);
-})
-
-
-
-
-var milkItem = Number(prompt("Enter number of milk"));
-    console.log(typeof milkItem)
-
-if(isNaN(milkItem)){
-    alert("Error: Please enter a valid number, not letters!")
-}else {
-    function getMilk(milk){
-    var costPerMilk = 5;
-    return milk * costPerMilk;
-}
-var numOfMilk  = getMilk(milkItem);
-    console.log(numOfMilk)
-    alert(`Number of Milk is ${milkItem} and Total Price of Milk is: $${numOfMilk}`)
+function add(num1, num2){
+    return num1 + num2;
 }
 
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
 
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function calculator(num1, num2, calculate){
+    return calculate(num1, num2)
+}
+
+console.log(calculator(5,2, divide))
