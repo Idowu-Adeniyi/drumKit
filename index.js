b8ada8d8a088de365b49fba585c3e643e8865b25
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2026-05-23 18:06:23 -0400

    made changes to code

--- index.js
+++ index.js
@@ -14,36 +14,34 @@ document.addEventListener("keydown", (e)=>{
                }
         }
 
-        
-        
      if(e.key === "w"){
         var audio = new Audio ("sounds/tom-1.mp3");
            audio.play();
-           console.log(e.key)
+          
      }else if(e.key === "a"){
          var audio = new Audio ("sounds/tom-2.mp3");
            audio.play();
-           console.log(e.key)     
+              
      }else if(e.key === "s"){
          var audio = new Audio ("sounds/tom-3.mp3");
            audio.play();
-           console.log(e.key)     
+              
      }else if(e.key === "d"){
          var audio = new Audio ("sounds/tom-4.mp3");
            audio.play();
-           console.log(e.key)     
+               
      }else if(e.key === "j"){
          var audio = new Audio ("sounds/crash.mp3");
            audio.play();
-           console.log(e.key)     
+               
      }else if(e.key === "k"){
          var audio = new Audio ("sounds/kick-bass.mp3");
            audio.play();
-           console.log(e.key)     
+             
      }else if(e.key === "l"){
          var audio = new Audio ("sounds/snare.mp3");
            audio.play();
-           console.log(e.key)     
+              
      }
   
 })
@@ -60,8 +58,6 @@ drums.forEach((drum)=>{
         if (key === "w" || key === "a" || key === "s" || key === "d" || key === "j" || key === "k" || key === "l") {
             e.target.style.color = "blue";
         }
-
-        
 
 
      if(e.target.innerHTML == "w"){
